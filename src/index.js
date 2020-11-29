3fe4266d8efd83fdaeff62ebe9e2eb427ed0c2fd
schreyerpeter <pschreyer@weedmaps.com>
2020-11-28 18:56:06 -0800

    Use Firebase CDN

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyA_6I6pdNNaQ0M4ENW6bT4Xq2qCOWMHd1s',
-  authDomain: 'stockly-48f43.firebaseapp.com',
-  databaseURL: 'https://stockly-48f43.firebaseio.com',
-  projectId: 'stockly-48f43',
-  storageBucket: 'stockly-48f43.appspot.com',
-  messagingSenderId: '766122554836',
-  appId: '1:766122554836:web:0e136c6a0afa9dcb2f1241',
-  measurementId: 'G-LEJ0PNSLMT',
-};
-
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
